612340a40a01b13f1e332cb6b0ec191a46f0199e
Rexx3 <wx036382@gmail.com>
2022-06-08 02:29:41 -0700

    Add files via upload

--- eval.py
+++ eval.py
@@ -1,8 +1,20 @@
 from dataloader import *
 from model import *
-from visualizer import Base
+from visualizer import Base, CifVisualizer, CafVisualizer, CifHr
+from heads import Cif, Caf
+from model import get_coco_multihead
+from annotation import Annotation
+import pycocotools.coco
+from pycocotools.cocoeval import COCOeval
 
-def enumerated_dataloader(enumerated_dataloader):
+def processor(model, processed_image_batch, device):
+	fbatch = fields_batch(model, processed_image_batch, device=device)
+	fbatch = decode(fbatch[0])
+	result = Dpool.starmap(mappable_annotations, zip(fbatch, processed_image_batch, [None for _ in fbatch]))
+	return result
+
+
+def enumerated_dataloader(enumerated_dataloader, model, device):
     """Predict from an enumerated dataloader."""
     for batch_i, item in enumerated_dataloader:
         if len(item) == 3:
@@ -10,10 +22,10 @@ def enumerated_dataloader(enumerated_dataloader):
             image_batch = [None for _ in processed_image_batch]
         elif len(item) == 4:
             image_batch, processed_image_batch, gt_anns_batch, meta_batch = item
-        if self.visualize_processed_image:
-            visualizer.Base.processed_image(processed_image_batch[0])
+        # if self.visualize_processed_image:
+        #     Base.processed_image(processed_image_batch[0])
 
-        pred_batch = self.processor.batch(self.model, processed_image_batch, device=self.device)
+        pred_batch = processor(model, processed_image_batch, device=device)
         # self.last_decoder_time = self.processor.last_decoder_time
         # self.last_nn_time = self.processor.last_nn_time
         # self.total_decoder_time += self.processor.last_decoder_time
@@ -28,9 +40,8 @@ def enumerated_dataloader(enumerated_dataloader):
             # load the original image if necessary
             # if self.visualize_image:
             #     visualizer.Base.image(image, meta=meta)
-
-            pred = [ann.inverse_transform(meta) for ann in pred]
-            gt_anns = [ann.inverse_transform(meta) for ann in gt_anns]
+            pred = [pred]
+            gt_anns = [gt_anns]
 
             # if self.json_data:
             #     pred = [ann.json_data() for ann in pred]
@@ -72,7 +83,8 @@ def accumulate(predictions, image_meta, *, ground_truth=None):
     #     self._stats(image_annotations, [image_id])
     #     LOG.debug(image_meta)
 
-    self.predictions += image_annotations
+    predictions += image_annotations
+    return predictions
 
 def fields_batch(model, image_batch, device=None):
 
@@ -106,19 +118,126 @@ def fields_batch(model, image_batch, device=None):
 
     return heads
 
+class Dpool():
+    @staticmethod
+    def starmap(f, iterable):
+        return [f(*i) for i in iterable]
+
+def mappable_annotations(fields, debug_image, gt_anns):
+    if debug_image is not None:
+        Base.processed_image(debug_image)
+    if gt_anns is not None:
+        Base.ground_truth(gt_anns)
+
+    return fields
+
+def decode(fields, initial_annotations=None):
+	headsmeta = get_coco_multihead()
+	cif_metas = [headsmeta[0]]
+	cif_visualizers = [CifVisualizer(m) for m in cif_metas]
+	caf_metas = [headsmeta[1]]
+	caf_visualizers = [CafVisualizer(m) for m in caf_metas]
+	cifhr_visualizers = [
+                CifHr(stride=meta.stride, field_names=meta.keypoints)
+                for meta in cif_metas
+            ]
+	if not initial_annotations:
+	    initial_annotations_t = None
+	    initial_ids_t = None
+	else:
+	    initial_annotations_t = torch.empty(
+	        (len(initial_annotations), cif_metas[0].n_fields, 4))
+	    initial_ids_t = torch.empty((len(initial_annotations),), dtype=torch.int64)
+	    for i, (ann_py, ann_t) in enumerate(zip(initial_annotations, initial_annotations_t)):
+	        for f in range(len(ann_py.data)):
+	            ann_t[f, 0] = float(ann_py.data[f, 2])
+	            ann_t[f, 1] = float(ann_py.data[f, 0])
+	            ann_t[f, 2] = float(ann_py.data[f, 1])
+	            ann_t[f, 3] = float(ann_py.joint_scales[f])
+	        initial_ids_t[i] = getattr(ann_py, 'id_', -1)
+	    LOG.debug('initial annotations = %d', initial_annotations_t.size(0))
+
+	for vis, meta in zip(cif_visualizers, cif_metas):
+		vis.predicted(fields[0])
+	for vis, meta in zip(caf_visualizers, caf_metas):
+	    vis.predicted(fields[1])
+	cpp_decoder = torch.classes.openpifpaf_decoder.CifCaf(
+            len(cif_metas[0].keypoints),
+            torch.LongTensor(caf_metas[0].skeleton) - 1,
+        )
+	annotations, annotation_ids = cpp_decoder.call_with_initial_annotations(
+	    fields[0],
+	    cif_metas[0].stride,
+	    fields[1],
+	    caf_metas[0].stride,
+	    initial_annotations_t,
+	    initial_ids_t,
+	)
+	for vis in cifhr_visualizers:
+	    fields, low = cpp_decoder.get_cifhr()
+	    vis.predicted(fields, low)
+	annotations_py = []
+	score_weights = cif_metas[0].score_weights
+	for ann_data, ann_id in zip(annotations, annotation_ids):
+
+	    ann = Annotation(cif_metas[0].keypoints,
+	                     caf_metas[0].skeleton,
+	                     score_weights=score_weights)
+	    ann.data[:, :2] = ann_data[:, 1:3]
+	    ann.data[:, 2] = ann_data[:, 0]
+	    ann.joint_scales[:] = ann_data[:, 3]
+	    if ann_id != -1:
+	        ann.id_ = int(ann_id)
+	    annotations_py.append(ann)
+
+	LOG.info('annotations %d: %s',
+	         len(annotations_py),
+	         [np.sum(ann.data[:, 2] > 0.1) for ann in annotations_py])
+	return annotations_py
+
+
+
 def main():
 	test_loader = Cocodata(args).eval_loader()
 	checkpoint = torch.load(args.checkpoint)
 	model = checkpoint['model'].cuda()
 	model.eval()
 	device = torch.device('cuda')
-	for batch_i, item in enumerate(test_loader):
-		fbatch = fields_batch(model, item[0], device=device)
-		for f in fbatch:
-			img = Base.processed_image(f)
-			print(type(img))
-		print(item[1].inverse_transform(item[2][0]))
-		metric = accumulate(item[0][0], item[2][0], ground_truth = item[1])
+	pred_loader = enumerated_dataloader(enumerate(test_loader), model, device)
+	predictions = []
+	i = 0
+	image_ids = []
+	for image_i, (pred, gt_anns, image_meta) in enumerate(pred_loader):
+		i+=1
+		image_ids.append(image_i)
+		predictions += accumulate(pred, image_meta, ground_truth = gt_anns)
+		if i> 30:
+			break
+
+
+	predictions = [
+            {k: v for k, v in annotation.items()
+             if k in ('image_id', 'category_id', 'keypoints', 'score')}
+            for annotation in predictions
+            if type(annotation) == dict
+        ]
+	coco = pycocotools.coco.COCO(annotation_file='C:/Users/wx036/Desktop/UCSD/ECE228/finalproject/data-mscoco/annotations/person_keypoints_val2017.json')
+	coco_eval = coco.loadRes(predictions)
+
+	myeval = COCOeval(coco, coco_eval, iouType='keypoints')
+	category_ids = [1]
+	if category_ids:
+	    myeval.params.catIds = category_ids
+	# if self.keypoint_oks_sigmas is not None:
+	#     self.eval.params.kpt_oks_sigmas = np.asarray(self.keypoint_oks_sigmas)
+
+	if image_ids is not None:
+	    print('image ids', image_ids)
+	    myeval.params.imgIds = image_ids
+	myeval.evaluate()
+	myeval.accumulate()
+	myeval.summarize()
+	print(myeval.stats)
 	    
 
 if __name__=='__main__':
